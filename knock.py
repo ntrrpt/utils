72b8ec5788eb3d0f652c58b99b9792aa4b949dd1
nt <>
2025-09-03 08:49:33 +0300

    ruff

--- knock.py
+++ knock.py
@@ -5,35 +5,36 @@ import sys
 import subprocess as sp
 
 # sequence that must be performed
-KNOCK_SEQUENCE = [5464, 4356, 1234, 5464, 1234, 8888] 
+KNOCK_SEQUENCE = [5464, 4356, 1234, 5464, 1234, 8888]
 KNOCK_PORTS = list(set(KNOCK_SEQUENCE))
 
 # time allocated for completing the entire sequence
-SEQUENCE_TIMEOUT = 15 
+SEQUENCE_TIMEOUT = 15
 
 # time after the ports are closed again
 EXPIRES_IN = 60 * 2
 
 UNLOCK_PORTS = [
-    3923, # copyparty
-    3389, # rdp
-    5900  # vnc
+    3923,  # copyparty
+    3389,  # rdp
+    5900,  # vnc
 ]
 
 # client data
 client_knocks = {}
 client_timeout = 0
 
-def win_port(port, protocols=['TCP', 'UDP']):
+
+def win_port(port, protocols=["TCP", "UDP"]):
     def run(c, ex=False):
         r = sp.run(
-            c, 
-            shell=True, 
+            c,
+            shell=True,
             text=True,
-            stdout=sp.PIPE, 
-            stderr=sp.PIPE, 
-            encoding='utf-8', 
-            errors='replace'
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+            encoding="utf-8",
+            errors="replace",
         )
         if ex and r.returncode != 0:
             print(f"[?] can't manage port {port}, are you admin?")
@@ -42,36 +43,36 @@ def win_port(port, protocols=['TCP', 'UDP']):
     block = port < 0
     port = abs(port)
 
-    fw = ['netsh', 'advfirewall', 'firewall']
-    fw.insert(0, 'sudo') # choco install gsudo
+    fw = ["netsh", "advfirewall", "firewall"]
+    fw.insert(0, "sudo")  # choco install gsudo
 
     for protocol in protocols:
         allow_name = f'name="!_allow_{protocol}:{port}"'
         block_name = f'name="!_block_{protocol}:{port}"'
 
         allow_cmd = [
-            'add', 
-            'rule', 
-            allow_name, 
-            'dir=in', 
-            'action=allow', 
-            f'protocol={protocol}', 
-            f'localport={port}'
+            "add",
+            "rule",
+            allow_name,
+            "dir=in",
+            "action=allow",
+            f"protocol={protocol}",
+            f"localport={port}",
         ]
 
         block_cmd = [
-            'add', 
-            'rule', 
-            block_name, 
-            'dir=in', 
-            'action=block', 
-            'remoteip=0.0.0.0-192.167.255.255,192.169.0.0-255.255.255.255', # lan
-            f'protocol={protocol}', 
-            f'localport={port}'
+            "add",
+            "rule",
+            block_name,
+            "dir=in",
+            "action=block",
+            "remoteip=0.0.0.0-192.167.255.255,192.169.0.0-255.255.255.255",  # lan
+            f"protocol={protocol}",
+            f"localport={port}",
         ]
 
         # recreate allow rule
-        run(fw + ['delete', 'rule', allow_name])
+        run(fw + ["delete", "rule", allow_name])
         run(fw + allow_cmd, True)
 
         if block:
@@ -81,16 +82,18 @@ def win_port(port, protocols=['TCP', 'UDP']):
 
         else:
             # remove block rule (allow)
-            run(fw + ['delete', 'rule', block_name])
+            run(fw + ["delete", "rule", block_name])
             print(f"[+] {protocol} {port}")
 
-port_manage = win_port # todo iptables
+
+port_manage = win_port  # todo iptables
+
 
 def listen_on_port(port):
     global client_timeout
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    sock.bind(('', port))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", port))
     print(f"[!] lisening {port}")
 
     while True:
@@ -103,23 +106,24 @@ def listen_on_port(port):
         now = time.time()
 
         # clean old attempts
-        knocks = [k for k in knocks if now - k[1] < SEQUENCE_TIMEOUT] 
+        knocks = [k for k in knocks if now - k[1] < SEQUENCE_TIMEOUT]
 
         knocks.append((port, now))
         client_knocks[ip] = knocks
 
         # seq checking
         seq = [k[0] for k in knocks]
-        if seq[-len(KNOCK_SEQUENCE):] == KNOCK_SEQUENCE:
+        if seq[-len(KNOCK_SEQUENCE) :] == KNOCK_SEQUENCE:
             print(f"[!] valid sequence from {ip}!")
-            client_knocks[ip] = [] # reset after success attempt
-            
+            client_knocks[ip] = []  # reset after success attempt
+
             if not client_timeout:
                 print(f"[!] opening {UNLOCK_PORTS}")
                 for p in UNLOCK_PORTS:
                     port_manage(p)
 
             client_timeout = now + EXPIRES_IN
+
 
 def check_expired():
     global client_timeout
@@ -129,18 +133,15 @@ def check_expired():
             port_manage(-p)
         client_timeout = 0
 
+
 if __name__ == "__main__":
     for p in UNLOCK_PORTS:
         port_manage(-p)
 
     for p in KNOCK_PORTS:
-        port_manage(p, ['UDP'])
-        
-        threading.Thread(
-            target=listen_on_port, 
-            args=(p,), 
-            daemon=True
-        ).start()
+        port_manage(p, ["UDP"])
+
+        threading.Thread(target=listen_on_port, args=(p,), daemon=True).start()
 
     try:
         while True:
@@ -148,12 +149,12 @@ if __name__ == "__main__":
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print('[!] stopping')
+        print("[!] stopping")
         for p in UNLOCK_PORTS:
             port_manage(-p)
         for p in KNOCK_PORTS:
-            port_manage(-p, ['UDP'])
-            
+            port_manage(-p, ["UDP"])
+
 """
 function knock() { nping --udp --count 1 --data-length 1 --dest-port $1 192.168.0.100 }
 
